935760ec9bcad01f27b528f40bc8551c5d07c8a8
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2021-04-23 14:51:37 +0100

    Added style changes to the header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -11,21 +13,27 @@ function Header() {
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
         {/* Logo */}
       </div>
 
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineOne">Sign In</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineOne">Orders</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineOne">Prime</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
