daa94a5698788f663eec7c4c63740c6b5171be31
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2021-04-29 18:07:26 +0100

    added cloud firestore database to the backend

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./Components/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./Components/Payment";
+import Orders from "./Components/Orders";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
@@ -40,6 +41,11 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+
           <Route path="/login">
             <Login />
           </Route>
